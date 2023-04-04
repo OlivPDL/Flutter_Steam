d500a3bdd6582052a96e85a6a5851a917b29a09a
OlivPDL <pasdeloupolivier@gmail.com>
2023-04-04 17:37:32 +0200

    front (un peu)

--- lib/login.dart
+++ lib/login.dart
@@ -11,6 +11,7 @@ import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -28,132 +29,149 @@ class _LoginPageState extends State<LoginPage> {
       constraints: const BoxConstraints.expand(),
       decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/ImageBackground.png"), fit: BoxFit.cover)),
-
-
-
+              image: AssetImage("assets/ImageBackground.png"),
+              fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-      body: Padding(
-        padding: EdgeInsets.all(30.10),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  labelStyle: TextStyle(
-                    color: Colors.white, // changez la couleur ici
+        body: Padding(
+          padding: EdgeInsets.all(30.10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 107),
+                    child: Text('Bienvenue !',
+                        style:TextStyle(
+                          fontFamily: 'GoogleSans',
+                          fontSize: 30.58,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700
+                        ),
+                          ),//GoogleFonts.getFont('Google Sans', fontSize: 30,)
                   ),
                 ),
-              ),
-              TextFormField(
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-                controller: _passwordController,
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'Mot de passe',
-                  labelStyle: TextStyle(
-                    color: Colors.white, // changez la couleur ici
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        try {
-                          UserCredential userCredential = await FirebaseAuth
-                              .instance
-                              .signInWithEmailAndPassword(
-                            email: _emailController.text,
-                            password: _passwordController.text,
-                          );
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => TopGamesScreen()),
-                          );
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            print('No user found for that email.');
-                          } else if (e.code == 'wrong-password') {
-                            print('Wrong password provided for that user.');
-                          }
-                        }
+                Center(
+                  
+                  child: TextFormField(
+
+                    style: TextStyle(
+                      color: Colors.white,
+                      backgroundColor: Color(0XFF1E262C),
+                    ),
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
                       }
+                      return null;
                     },
-                    child: Text(
-                      'Se connecter',
-                      style: TextStyle(
-                        color: Colors.white,
+                    decoration: InputDecoration(
+                      labelText: 'E-mail',
+                      labelStyle: TextStyle(
+                        color: Colors.white, // changez la couleur ici
                       ),
                     ),
                   ),
-                  TextButton(
+                ),
+                TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  controller: _passwordController,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Mot de passe',
+                    labelStyle: TextStyle(
+                      color: Colors.white, // changez la couleur ici
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          try {
+                            UserCredential userCredential = await FirebaseAuth
+                                .instance
+                                .signInWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            );
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => TopGamesScreen()),
+                            );
+                          } on FirebaseAuthException catch (e) {
+                            if (e.code == 'user-not-found') {
+                              print('No user found for that email.');
+                            } else if (e.code == 'wrong-password') {
+                              print('Wrong password provided for that user.');
+                            }
+                          }
+                        }
+                      },
+                      child: Text(
+                        'Se connecter',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage()),
+                        );
+                      },
+                      child: Text(
+                        'Mot de passe oublié',
+                        style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                Center(
+                  child: TextButton(
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgotPasswordPage()),
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
                       );
                     },
                     child: Text(
-                      'Mot de passe oublié',
+                      'Créer un nouveau compte',
                       style: TextStyle(
                         color: Colors.white,
-                        decoration: TextDecoration.underline,
                       ),
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpPage()),
-                    );
-                  },
-                  child: Text(
-                    'Créer un nouveau compte',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
 }
